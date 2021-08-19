c408d52baf10c139193dcdee4b48d2bf9ad83a80
Nya Candy <dev@candinya.com>
2021-08-19 17:34:02 +0800

    (wip)

---
+++ src/components/Avatar.vue
@@ -0,0 +1,66 @@
+<template>
+    <label class="avatar" :class="size">
+        <img class="image" :src="newUrl || url" v-show="newUrl || url" alt="Avatar Upload" />
+        <span
+            class="plus"
+            :class="{
+                fill: newUrl || url,
+            }"
+        >
+            <i class="bx bx-plus" />
+        </span>
+        <input type="file" class="hidden" accept="image/*" @change="preview" />
+    </label>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component';
+import ipfs from '@/common/ipfs';
+
+@Options({
+    props: {
+        size: String,
+        url: String,
+    },
+    components: {},
+})
+export default class Avatar extends Vue {
+    file?: File;
+    newUrl: string = '';
+
+    preview(evt: any) {
+        this.file = evt.target?.files?.[0];
+        this.newUrl = URL.createObjectURL(this.file);
+    }
+
+    async upload() {
+        if (this.file) {
+            return ipfs.upload(this.file);
+        } else {
+            return null;
+        }
+    }
+}
+</script>
+
+<style lang="postcss" scoped>
+@layer components {
+    .avatar {
+        @apply flex justify-center items-center cursor-pointer rounded overflow-hidden relative;
+
+        &.sm {
+            @apply w-14 h-14;
+        }
+        &.md {
+            @apply w-18 h-18;
+        }
+        &.lg {
+            @apply w-30 h-30;
+        }
+
+        .image {
+            @apply object-contain object-center absolute left-0 top-0 w-full h-full;
+        }
+    }
+}
+</style>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,14 +1,23 @@
 <template>
     <h1>Test page</h1>
+    <Input :is-single-line="false" v-model="value" />
+    <span>
+        {{ value }}
+    </span>
+    <Avatar />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import Input from '@/components/Input.vue';
+import Avatar from '@/components/Avatar.vue';
 
 @Options({
-    components: {},
+    components: { Avatar, Input },
 })
-export default class Test extends Vue {}
+export default class Test extends Vue {
+    value: String = 'value';
+}
 </script>
 
 <style></style>
