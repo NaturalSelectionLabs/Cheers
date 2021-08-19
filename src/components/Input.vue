8511d039f51cc7273eab96b9e7470aa5249a28ab
Nya Candy <dev@candinya.com>
2021-08-19 16:10:05 +0800

    add: Input component

---
+++ src/components/Input.vue
@@ -0,0 +1,51 @@
+<template>
+    <input
+        v-if="isSingleLine"
+        type="text"
+        class="input"
+        :placeholder="placeholder"
+        :style="style"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+    />
+    <textarea
+        v-else
+        class="input"
+        :placeholder="placeholder"
+        :style="style"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+    />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    components: {},
+    props: {
+        modelValue: String,
+        placeholder: String,
+        width: String,
+        isSingleLine: Boolean,
+    },
+    emits: ['update:modelValue'],
+})
+export default class Input extends Vue {}
+</script>
+
+<style>
+@layer components {
+    .input {
+        @apply box-border font-extralight pl-3 pr-5 rounded-xl border-none resize-none outline-none text-sm; /* color && bg */
+
+        &:focus {
+            @apply pl-4; /* bg */
+        }
+
+        &::placeholder {
+            @apply font-medium; /* color*/
+        }
+    }
+}
+</style>
