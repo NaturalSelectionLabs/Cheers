4c9d57e204065119dbdceea4eafa1ed27fa2a767
DIYgod <i@diygod.me>
2021-08-24 02:53:31 +0100

    feat: setup pages

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Index from '@/views/Index.vue';
+import Setup from '@/views/Setup.vue';
+import SetupAccounts from '@/views/SetupAccounts.vue';
+import SetupNfts from '@/views/SetupNfts.vue';
 
 import Test from '@/views/Test.vue';
 
@@ -8,6 +11,18 @@ const routes = [
     {
         path: '/',
         component: Index,
+    },
+    {
+        path: '/setup',
+        component: Setup,
+    },
+    {
+        path: '/setup/accounts',
+        component: SetupAccounts,
+    },
+    {
+        path: '/setup/nfts',
+        component: SetupNfts,
     },
     {
         path: '/test',
