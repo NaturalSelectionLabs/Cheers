f9b464e7fcb400e689d730b1b92b4b93340db0bd
TuziMoe <a@tuzi.moe>
2021-08-29 06:17:59 +1200

    chore: vuegtag

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,19 @@ import '@/assets/main.css';
 import 'boxicons/css/boxicons.min.css'; // boxicons
 import App from './App.vue';
 import router from './router';
+import VueGtag from 'vue-gtag';
 
 const app = createApp(App);
 
 app.use(router);
+
+app.use(
+    VueGtag,
+    {
+        config: { id: 'G-740EEBM023' },
+        pageTrackerScreenviewEnabled: true,
+    },
+    router,
+);
 
 app.mount('#app');
