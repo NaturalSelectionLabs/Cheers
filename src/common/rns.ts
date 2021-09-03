b0b6ff0f6402f0f38ba9839a25bb1ff5163be7d2
Nya Candy <dev@candinya.com>
2021-09-04 01:43:30 +0800

    fix: no metamask on mobile browser

--- src/common/rns.ts
+++ src/common/rns.ts
@@ -12,7 +12,7 @@ async function callRNSContract<T>(
     method: string,
     ...args: any
 ): Promise<T> {
-    (window as any).ethereum.enable();
+    (window as any).ethereum?.enable();
     let provider: ethers.providers.Web3Provider | ethers.providers.InfuraProvider;
     let signer; // TODO
     if (providerType === 'web3') {
@@ -82,7 +82,7 @@ export default {
         const reverseNode = '0x91d1777781884d03a6757a803996e38de2a42967fb37eeaca72729271025a9e2';
         const addrHex = sha3HexAddress(addr.toLowerCase());
         const node = utils.keccak256(utils.defaultAbiCoder.encode(['bytes32', 'bytes32'], [reverseNode, addrHex]));
-        return callRNSContract<string>('resolver', 'web3', speed, 'name', node);
+        return callRNSContract<string>('resolver', 'infura', speed, 'name', node);
     },
     name2Addr(name: string, speed: SPEED = 'average') {
         return callRNSContract<string>('resolver', 'infura', speed, 'addr', utils.namehash(name));
