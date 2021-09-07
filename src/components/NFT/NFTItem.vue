0ce9ef55ac113b2314ac47c1209131d5e06a104f
yzhe819 <yzhe819@aucklanduni.ac.nz>
2021-09-08 01:02:44 +1200

    fix: NFT item display issue

--- src/components/NFT/NFTItem.vue
+++ src/components/NFT/NFTItem.vue
@@ -1,39 +1,46 @@
 <template>
-    <video
-        v-if="imageUrl?.endsWith('mp4') || imageUrl?.endsWith('mov')"
+    <div
         :style="{
             width: size + 'px',
             height: size + 'px',
         }"
-        :src="imageUrl"
-        class="nft-item"
-        autoplay
-        loop
-        muted
-    />
-    <model-viewer
-        v-else-if="imageUrl?.endsWith('glb')"
-        :style="{
-            width: size + 'px',
-            height: size + 'px',
-        }"
-        :src="imageUrl"
-        class="nft-item"
-        ar
-        ar-modes="webxr scene-viewer quick-look"
-        environment-image="neutral"
-        auto-rotate
-        camera-controls
-    />
-    <img
-        v-else
-        :style="{
-            width: size + 'px',
-            height: size + 'px',
-        }"
-        :src="imageUrl"
-        class="nft-item"
-    />
+    >
+        <video
+            v-if="imageUrl?.endsWith('mp4') || imageUrl?.endsWith('mov')"
+            :src="imageUrl"
+            class="nft-item"
+            :style="{
+                width: size + 'px',
+                height: size + 'px',
+            }"
+            autoplay
+            loop
+            muted
+        />
+        <model-viewer
+            v-else-if="imageUrl?.endsWith('glb')"
+            :src="imageUrl"
+            class="nft-item"
+            :style="{
+                width: size + 'px',
+                height: size + 'px',
+            }"
+            ar
+            ar-modes="webxr scene-viewer quick-look"
+            environment-image="neutral"
+            auto-rotate
+            camera-controls
+        />
+        <img
+            v-else
+            :src="imageUrl"
+            class="nft-item"
+            :style="{
+                width: size + 'px',
+                height: size + 'px',
+            }"
+        />
+    </div>
 </template>
 
 <script lang="ts">
