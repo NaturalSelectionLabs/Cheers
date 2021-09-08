5bbb2a6068216056b04f162d5686b44055587f76
Nya Candy <dev@candinya.com>
2021-09-08 21:20:09 +0800

    chore: overflow hidden & ellipsis

--- src/components/GitcoinCard.vue
+++ src/components/GitcoinCard.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="w-full flex flex-row justify-start items-center rounded rounded-2xl bg-white shadow-gitcoin-md">
         <img :src="imageUrl" class="w-32 h-32 rounded rounded-2xl m-0.5" />
-        <div class="w-45 md:w-60 pl-10">
-            <div class="mb-2 font-semibold text-lg">{{ name }}</div>
+        <div class="w-45 md:w-60 pl-5">
+            <div class="mb-2 font-semibold text-lg overflow-hidden overflow-ellipsis">
+                {{ name }}
+            </div>
             <div class="w-full flex flex-row gap-x-6 overflow-y-auto">
                 <div>
                     <div class="font-medium">{{ contrib }}</div>
                     <div>Contrib</div>
+                </div>
+                <div>
+                    <div class="font-medium">{{ contrib }}</div>
+                    <div>ETH</div>
                 </div>
             </div>
         </div>
