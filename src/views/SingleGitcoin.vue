7b47cd74e57b84be1752fe44865f89015c6d7069
Atlas Quan <atlaschuen@uchicago.edu>
2021-09-12 13:10:24 +0800

    fix: days count on tsp

--- src/views/SingleGitcoin.vue
+++ src/views/SingleGitcoin.vue
@@ -196,7 +196,7 @@ export default class SingleGitcoin extends Vue {
         const date1: any = new Date(timeStamp * 1000);
         const date2: any = Date.now();
         const diffTime = Math.abs(date2 - date1);
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
         const diffHours = Math.ceil((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         if (diffDays == 0) {
             return diffHours + ' hours ago';
