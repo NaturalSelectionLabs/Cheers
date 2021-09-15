115813ae66604218a225e3222822652c486a2ba7
Nya Candy <dev@candinya.com>
2021-09-15 11:25:22 +0800

    fix: Index page logo style

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="onboarding h-full text-center bg-cover bg-fixed flex items-center justify-center bg-pass3gradient">
-        <div class="body px-4 h-2/3 flex flex-col justify-start items-center gap-56">
+        <div class="body px-4 h-2/3 flex flex-col justify-start items-center justify-between">
             <div class="logo-container w-50 h-50 bg-pass3logo bg-center bg-contain bg-no-repeat"></div>
             <div class="leading-17.5 text-white w-83.5 text-2xl mx-auto">
                 <Button
