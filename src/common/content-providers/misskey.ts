26421231bd36961fa8db25f0d1fa6cd4158b7b93
Nya Candy <dev@candinya.com>
2021-09-22 01:38:32 +0800

    chore: refractor API

--- src/common/content-providers/misskey.ts
+++ src/common/content-providers/misskey.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
-function splice(account: string) {
+const userInfoMap = new Map();
+
+function spliceAccountString(account: string) {
     const splices = account.split('@');
     if (splices.length < 2) {
-        return false;
+        return {
+            instance: '',
+            username: '',
+        };
     }
     const username = splices[0];
     const instance = splices[1];
@@ -13,69 +18,105 @@ function splice(account: string) {
     };
 }
 
-interface FeedItem {
-    content_html: string; // Full content
-    url: string; // Note link
-    title: string; // New note by {{ user.name }}
-    date_modified: string; // Timestamp
+async function getUser(account: string, refresh: boolean = false) {
+    const { username, instance } = spliceAccountString(account);
+    if (!refresh && userInfoMap.has(account)) {
+        return userInfoMap.get(account);
+    }
+    // Else
+    try {
+        const res = await axios.post(`https://${instance}/api/users/show`, {
+            username: username,
+        });
+        if (res.data) {
+            userInfoMap.set(account, res.data);
+        }
+        return res.data;
+    } catch (e) {
+        console.log(e);
+    }
+    // If fails
+    return null;
+}
+
+// Only necessary parts for type
+
+export interface Note {
+    id: string;
+    createdAt: string;
+    text: string;
+    renoteCount: number;
+    repliesCount: number;
+    reactions: {
+        [key: string]: number;
+    };
+    files: File[];
+}
+
+interface File {
+    id: string;
+    type: string;
+    url: string;
+    thumbnailUrl: string;
 }
 
 export default {
     verify: async (account: string, rns: string, ethAddress: string): Promise<boolean> => {
-        const { username, instance } = splice(account);
-        try {
-            const res = await axios.post(`https://${instance}/api/users/show`, {
-                username: username,
-            });
-            const u = res.data;
-            if (u) {
-                // Phase 1: in "name"
-                if ((rns && u.name?.includes(rns)) || u.name?.includes(ethAddress)) {
-                    return true;
-                }
+        const u = await getUser(account);
+        if (u) {
+            // Phase 1: in "name"
+            if ((rns && u.name?.includes(rns)) || u.name?.includes(ethAddress)) {
+                return true;
+            }
 
-                // Phase 2: in "description"
-                if ((rns && u.description?.includes(rns)) || u.description?.includes(ethAddress)) {
-                    return true;
-                }
+            // Phase 2: in "description"
+            if ((rns && u.description?.includes(rns)) || u.description?.includes(ethAddress)) {
+                return true;
+            }
 
-                // Phase 3: in "fields"
-                if (u.fields?.length) {
-                    for (const pair of u.fields) {
-                        if ((rns && pair.value?.includes(rns)) || pair.value?.includes(ethAddress)) {
-                            return true;
-                        }
-                    }
-                }
-
-                // Phase 4: in "pinnedNotes"
-                if (u.pinnedNotes?.length) {
-                    for (const note of u.pinnedNotes) {
-                        if ((rns && note.text?.includes(rns)) || note.text?.includes(ethAddress)) {
-                            return true;
-                        }
+            // Phase 3: in "fields"
+            if (u.fields?.length) {
+                for (const pair of u.fields) {
+                    if ((rns && pair.value?.includes(rns)) || pair.value?.includes(ethAddress)) {
+                        return true;
                     }
                 }
             }
-        } catch (e) {
-            console.log(e);
-            return false;
+
+            // Phase 4: in "pinnedNotes"
+            if (u.pinnedNotes?.length) {
+                for (const note of u.pinnedNotes) {
+                    if ((rns && note.text?.includes(rns)) || note.text?.includes(ethAddress)) {
+                        return true;
+                    }
+                }
+            }
         }
 
         // Else: not verified
         return false;
     },
-    get: async (account: string): Promise<FeedItem[]> => {
-        const { username, instance } = splice(account);
-        try {
-            const res = await axios.get(`https://${instance}/@${username}.json`);
-            const data = res.data;
-            if (data.items?.length) {
-                return <FeedItem[]>data.items;
+    getNotes: async (account: string, sinceDate?: string, untilDate?: string): Promise<Note[]> => {
+        const { instance } = spliceAccountString(account);
+        const u = await getUser(account);
+        if (u) {
+            try {
+                const reqData = {
+                    userId: u.id,
+                    sinceDate,
+                    untilDate,
+                    limit: 20,
+                    excludeNsfw: true,
+                };
+                const res = await axios.post(`https://${instance}/api/users/notes`, reqData);
+                if (res.data) {
+                    return <Note[]>res.data;
+                }
+            } catch (e) {
+                console.log(e);
             }
-        } catch (e) {
-            console.log(e);
         }
+        // Else or fail
         return [];
     },
 };
