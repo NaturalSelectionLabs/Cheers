4c831069eafa3ccf93b258b4a2440dc42dbfcf83
yzhe819 <yzhe819@aucklanduni.ac.nz>
2021-09-21 21:12:48 +1200

    chore: loot theme support

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -30,14 +30,14 @@ export default class Tooltip extends Vue {
 <style lang="postcss" scoped>
 @layer components {
     .tooltip {
-        @apply absolute transform -translate-x-1/2 inline-block top-0 left-1/2 rounded-sm px-2 py-1 text-primary-text bg-primary-link font-normal;
+        @apply absolute transform -translate-x-1/2 inline-block top-0 left-1/2 rounded-sm px-2 py-1 text-primary-text bg-primary-link font-normal border-link border-primary-link-border;
         left: calc(100% + 45px);
     }
 
     .tooltip::after {
         content: '';
         @apply absolute bottom-full left-0 -ml-2 top-3 border-solid border-4;
-        border-color: transparent var(--color-linkbutton-bg) transparent transparent;
+        border-color: transparent var(--color-sharebutton-border) transparent transparent;
     }
 }
 </style>
