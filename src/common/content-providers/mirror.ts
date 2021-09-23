72e137a62ae2539f5c4d6f2121cc2f3b1fa3c118
Nya Candy <dev@candinya.com>
2021-09-23 22:51:12 +0800

    chore: change back to prod hub

--- src/common/content-providers/mirror.ts
+++ src/common/content-providers/mirror.ts
@@ -8,9 +8,8 @@ export default {
     },
     get: async (address: string, sinceOffset: number = 0, untilTimeStamp: number = 0xffffffff): Promise<Content[]> => {
         try {
-            // const res = await axios.get(`${config.hubEndpoint}/asset-profile/${address}/mirror-xyz/?offset=${sinceOffset}&limit=${config.contentRequestLimit}&latest=${untilTimeStamp}`);
             const res = await axios.get(
-                `http://143.244.186.198:3000/asset-profile/${address}/mirror-xyz/?offset=${sinceOffset}&limit=${config.contentRequestLimit}&latest=${untilTimeStamp}`,
+                `${config.hubEndpoint}/asset-profile/${address}/mirror-xyz/?offset=${sinceOffset}&limit=${config.contentRequestLimit}&latest=${untilTimeStamp}`,
             );
             if (res.data?.status) {
                 return res.data.assets;
