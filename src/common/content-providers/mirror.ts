89bb5c3fa6d3c4aa85e4562f3be80a33a95db458
Nya Candy <dev@candinya.com>
2021-09-23 22:06:22 +0800

    chore: using real address

--- src/common/content-providers/mirror.ts
+++ src/common/content-providers/mirror.ts
@@ -10,7 +10,7 @@ export default {
         try {
             // const res = await axios.get(`${config.hubEndpoint}/asset-profile/${address}/mirror-xyz/?offset=${sinceOffset}&limit=${config.contentRequestLimit}&latest=${untilTimeStamp}`);
             const res = await axios.get(
-                `http://143.244.186.198:3000/asset-profile/0x9C159121CEEBF937E9bad98fd0c895d7f6038bd4/mirror-xyz/?key=f09f15D9034732172bC187Cb7e810dEA3A3CfF8c&offset=${sinceOffset}&limit=${config.contentRequestLimit}&latest=${untilTimeStamp}`,
+                `http://143.244.186.198:3000/asset-profile/${address}/mirror-xyz/?offset=${sinceOffset}&limit=${config.contentRequestLimit}&latest=${untilTimeStamp}`,
             );
             if (res.data?.status) {
                 return res.data.assets;
