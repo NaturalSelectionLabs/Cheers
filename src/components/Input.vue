746829d3f65da8ff8a6e8400180827e52db9481e
Nya Candy <dev@candinya.com>
2021-10-07 00:39:33 +0800

    chore: add prefix & suffix to Input component

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,29 +1,43 @@
 <template>
-    <input
-        v-if="isSingleLine"
-        type="text"
-        class="input"
+    <div
+        class="wrapper"
         :class="{
             error: $props.isError,
             'cursor-not-allowed': $props.isDisabled,
         }"
-        :placeholder="placeholder"
-        :value="modelValue"
-        @input="$emit('update:modelValue', $event.target.value)"
-        :disabled="$props.isDisabled"
-    />
-    <textarea
-        v-else
-        class="input textarea"
-        :class="{
-            error: $props.isError,
-            'cursor-not-allowed': $props.isDisabled,
-        }"
-        :placeholder="placeholder"
-        :value="modelValue"
-        @input="$emit('update:modelValue', $event.target.value)"
-        :disabled="$props.isDisabled"
-    />
+    >
+        <span v-if="$props.prefix" class="additional">
+            {{ $props.prefix }}
+        </span>
+        <input
+            v-if="isSingleLine"
+            type="text"
+            class="input"
+            :class="{
+                error: $props.isError,
+                'cursor-not-allowed': $props.isDisabled,
+            }"
+            :placeholder="placeholder"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+            :disabled="$props.isDisabled"
+        />
+        <textarea
+            v-else
+            class="input textarea"
+            :class="{
+                error: $props.isError,
+                'cursor-not-allowed': $props.isDisabled,
+            }"
+            :placeholder="placeholder"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+            :disabled="$props.isDisabled"
+        />
+        <span v-if="$props.suffix" class="additional">
+            {{ $props.suffix }}
+        </span>
+    </div>
 </template>
 
 <script lang="ts">
@@ -36,6 +50,8 @@ import { Options, Vue } from 'vue-class-component';
         isSingleLine: Boolean,
         isError: Boolean,
         isDisabled: Boolean,
+        prefix: String,
+        suffix: String,
     },
     emits: ['update:modelValue'],
 })
@@ -44,8 +60,16 @@ export default class Input extends Vue {}
 
 <style scoped lang="postcss">
 @layer components {
+    .wrapper {
+        @apply box-border font-extralight pl-5 pr-5 pt-3 pb-3 rounded-xl border-input border-input-border bg-input-bg flex;
+    }
+
+    .additional {
+        @apply flex;
+    }
+
     .input {
-        @apply box-border font-extralight pl-5 pr-5 pt-3 pb-3 rounded-xl border-input border-input-border resize-none outline-none bg-input-bg placeholder-black placeholder-opacity-20;
+        @apply border-input font-extralight border-input-border resize-none outline-none bg-input-bg placeholder-black placeholder-opacity-20 flex w-full;
     }
     .input::placeholder {
         font-weight: 500;
