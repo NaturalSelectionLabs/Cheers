9c77f6cbd42e71b44739cf95b9ffc5324e4da1fc
Nya Candy <dev@candinya.com>
2021-10-12 14:13:04 +0800

    remove: misskey unused part

--- src/common/content-providers/misskey.ts
+++ src/common/content-providers/misskey.ts
@@ -2,8 +2,6 @@ import axios from 'axios';
 import config from '@/config';
 import { Content } from './index';
 
-const userInfoMap = new Map();
-
 function spliceAccountString(account: string) {
     const splices = account.split('@');
     if (splices.length < 2) {
@@ -18,51 +16,6 @@ function spliceAccountString(account: string) {
         instance,
         username,
     };
-}
-
-async function getUser(account: string, refresh: boolean = false) {
-    const { username, instance } = spliceAccountString(account);
-    if (!username || !instance) {
-        return null;
-    }
-    if (!refresh && userInfoMap.has(account)) {
-        return userInfoMap.get(account);
-    }
-    // Else
-    try {
-        const res = await axios.post(`https://${instance}/api/users/show`, {
-            username: username,
-        });
-        if (res.data) {
-            userInfoMap.set(account, res.data);
-        }
-        return res.data;
-    } catch (e) {
-        console.log(e);
-    }
-    // If fails
-    return null;
-}
-
-// Only necessary parts for type
-
-export interface Note {
-    id: string;
-    createdAt: string;
-    text: string;
-    renoteCount: number;
-    repliesCount: number;
-    reactions: {
-        [key: string]: number;
-    };
-    files: File[];
-}
-
-interface File {
-    id: string;
-    type: string;
-    url: string;
-    thumbnailUrl: string;
 }
 
 export default {
