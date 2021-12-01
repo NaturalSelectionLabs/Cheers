799ab1fc2deb805214b56617e0c602749fef378d
Nya Candy <dev@candinya.com>
2021-12-02 00:30:41 +0800

    chore: refractor NFTItems

--- src/components/NFT/NFTItem.vue
+++ src/components/NFT/NFTItem.vue
@@ -4,25 +4,17 @@
         :class="{
             'w-10 h-10 rounded-sm': size === 'sm',
             'w-16 h-16 rounded': size === 'md',
-            'w-full aspect-w-1 aspect-h-1 rounded': size === 'auto',
+            'w-full aspect-w-1 aspect-h-1 rounded': size === 'auto' || viewType === 'model',
+            rounded: size === 'contain',
         }"
     >
         <video
-            v-if="
-                imageUrl?.endsWith('.mp4') ||
-                imageUrl?.endsWith('.mov') ||
-                imageUrl?.endsWith('.webm') ||
-                imageUrl?.endsWith('.mp3')
-            "
-            :src="imageUrl"
-            :poster="
-                posterUrl?.endsWith('.mp4') || posterUrl?.endsWith('.mov') || posterUrl?.endsWith('.webm')
-                    ? undefined
-                    : posterUrl
-            "
+            v-if="viewType === 'video'"
+            :src="mainUrl"
+            :poster="subUrl"
             class="nft-item"
             :class="[!isShowingDetails ? 'object-cover' : 'object-contain', size === 'sm' ? 'rounded-sm' : 'rounded']"
-            :autoplay="isShowingDetails || posterUrl?.endsWith('.mp4') || posterUrl?.endsWith('.mov')"
+            :autoplay="isShowingDetails"
             loop
             webkit-playsinline
             playsinline
@@ -30,23 +22,14 @@
             :controls="isShowingDetails"
         />
         <iframe
-            v-else-if="
-                (imageUrl?.endsWith('embed') ||
-                    imageUrl?.includes('farmhero.io') ||
-                    imageUrl?.includes('0xAdventures.com') ||
-                    imageUrl?.includes('crudefingers.com') ||
-                    imageUrl?.includes('artblocks.io') ||
-                    imageUrl?.endsWith('.html')) &&
-                isShowingDetails
-            "
-            :src="imageUrl"
+            v-else-if="viewType === 'website'"
+            :src="mainUrl"
             class="nft-item"
             :class="[!isShowingDetails ? 'object-cover' : 'object-contain', size === 'sm' ? 'rounded-sm' : 'rounded']"
         />
         <model-viewer
-            v-else-if="modelView"
-            :src="imageUrl"
-            :poster="posterUrl"
+            v-else-if="viewType === 'model'"
+            :src="mainUrl"
             class="nft-item"
             :class="[!isShowingDetails ? 'object-cover' : 'object-contain', size === 'sm' ? 'rounded-sm' : 'rounded']"
             ar-modes="webxr scene-viewer quick-look"
@@ -57,7 +40,7 @@
         />
         <img
             v-else
-            :src="showImageUrl"
+            :src="mainUrl"
             class="nft-item"
             :class="[!isShowingDetails ? 'object-cover' : 'object-contain', size === 'sm' ? 'rounded-sm' : 'rounded']"
         />
@@ -71,8 +54,8 @@ import config from '@/config';
 @Options({
     props: {
         size: String,
-        posterUrl: String, // This should be image URL
-        imageUrl: String, // This should be detailed URL
+        posterUrl: String, // This should be preview URL
+        imageUrl: String, // This should be detail URL
         isShowingDetails: Boolean,
     },
 })
@@ -80,26 +63,15 @@ export default class NFTItem extends Vue {
     size!: String;
     imageUrl!: String;
     posterUrl!: String;
-    defaultImage: String = config.defaultAvatar;
     isShowingDetails!: boolean;
+    subUrl: string | undefined;
 
-    get modelView() {
-        const result = (this.imageUrl?.endsWith('.glb') || this.imageUrl?.endsWith('.gltf')) && this.isShowingDetails;
-        if (result) {
-            import(/* webpackChunkName: "model-viewer" */ '@google/model-viewer');
-        }
-        return result;
-    }
-
-    get showImageUrl() {
-        // isShowingDetails ? imageUrl || posterUrl || defaultImage : posterUrl || imageUrl || defaultImage
-        if (this.isShowingDetails) {
-            // Prefer detailed URL
-            return this.handleIPFS(this.imageUrl || this.posterUrl || this.defaultImage);
-        } else {
-            // Prefer preview URL
-            return this.handleIPFS(this.posterUrl || this.imageUrl || this.defaultImage);
-        }
+    get mainUrl() {
+        return this.handleIPFS(
+            this.isShowingDetails
+                ? this.imageUrl || this.posterUrl || config.defaultAvatar
+                : this.posterUrl || this.imageUrl || config.defaultAvatar,
+        );
     }
 
     handleIPFS(url: String) {
@@ -107,6 +79,36 @@ export default class NFTItem extends Vue {
             return url.replace('ipfs://', 'https://cloudflare-ipfs.com/ipfs/');
         } else {
             return url;
+        }
+    }
+
+    get viewType(): 'video' | 'website' | 'model' | 'image' {
+        if (this.imageUrl?.endsWith('.mp4') || this.imageUrl?.endsWith('.mov') || this.imageUrl?.endsWith('.webm')) {
+            if (
+                !(
+                    this.posterUrl?.endsWith('.mp4') ||
+                    this.posterUrl?.endsWith('.mov') ||
+                    this.posterUrl?.endsWith('.webm')
+                )
+            ) {
+                this.subUrl = this.posterUrl.toString();
+            } else {
+                this.subUrl = undefined;
+            }
+            return 'video';
+        } else if (
+            this.imageUrl?.endsWith('.html') ||
+            this.imageUrl?.includes('farmhero.io') ||
+            this.imageUrl?.includes('0xAdventures.com') ||
+            this.imageUrl?.includes('crudefingers.com') ||
+            this.imageUrl?.includes('artblocks.io')
+        ) {
+            return 'website';
+        } else if (this.imageUrl?.endsWith('.gltf') || this.imageUrl?.endsWith('.glb')) {
+            import(/* webpackChunkName: "model-viewer" */ '@google/model-viewer');
+            return 'model';
+        } else {
+            return 'image';
         }
     }
 }
