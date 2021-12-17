e77a6a73fbe0b3847e3a90cc3100dacd0942d7c3
Nya Candy <dev@candinya.com>
2021-12-17 18:02:50 +0800

    fix: reconnect

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,14 @@
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import RSS3 from '@/common/rss3';
 
 @Options({})
-export default class App extends Vue {}
+export default class App extends Vue {
+    async mounted() {
+        await RSS3.reconnect();
+    }
+}
 </script>
 
 <style>
