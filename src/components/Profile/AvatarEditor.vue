53d02bb906e15a66db57632b98b181f62b9f9445
Nya Candy <dev@candinya.com>
2021-12-17 18:05:58 +0800

    fix: edit avatar image

--- src/components/Profile/AvatarEditor.vue
+++ src/components/Profile/AvatarEditor.vue
@@ -1,6 +1,6 @@
 <template>
     <label class="avatar" :class="size">
-        <img class="image" :src="newUrl || url" v-show="newUrl || url" alt="Avatar Upload" />
+        <ImgHolder class="image" :src="newUrl || url" v-show="newUrl || url" :isRounded="true" alt="Avatar Upload" />
         <div
             class="plus"
             :class="{
@@ -16,8 +16,10 @@
 <script lang="ts">
 import { Vue, Options } from 'vue-class-component';
 import ipfs from '@/common/ipfs';
+import ImgHolder from '@/components/Common/ImgHolder.vue';
 
 @Options({
+    components: { ImgHolder },
     props: {
         size: String,
         url: String,
