135b23135e54f249f3b483fc815ed9e2ef0f78d9
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-01-23 18:26:39 +1300

    chore: update menu icon color

--- src/components/Icons/Menu.vue
+++ src/components/Icons/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg :width="size" :height="size" fill="#0072FF" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <svg :width="size" :height="size" fill="#FED581" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <path
             d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"
         />
