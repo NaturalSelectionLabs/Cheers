d0806f1a9a424b9c33129f1de6d24b5d63b8d180
Nya Candy <dev@candinya.com>
2022-03-02 13:59:43 +0800

    fix: dont inherit scroll position

--- src/router.ts
+++ src/router.ts
@@ -138,6 +138,13 @@ if (!config.subDomain.isSubDomainMode) {
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { top: 0, left: 0 };
+        }
+    },
 });
 
 export default router;
