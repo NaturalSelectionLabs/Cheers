1307d5f8f7ebb42637759260b4d3f70173ce1f73
Nya Candy <dev@candinya.com>
2022-03-02 10:05:13 +0800

    feat: drop sentry for now

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import App from './App.vue';
 import router from './router';
 import VueGtag from 'vue-gtag';
 import Hotjar from 'vue-hotjar';
-import * as Sentry from '@sentry/vue';
-import { Integrations } from '@sentry/tracing';
+// import * as Sentry from '@sentry/vue';
+// import { Integrations } from '@sentry/tracing';
 
 const app = createApp(App);
 
@@ -26,20 +26,20 @@ app.use(Hotjar, {
 });
 
 /* IFTRUE_isNDevelop */
-Sentry.init({
-    app,
-    dsn: 'https://19adad3122ef486cbd3201112af00024@sentry.rss3.dev/2',
-    integrations: [
-        new Integrations.BrowserTracing({
-            routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-            tracingOrigins: [/^(.+\.)?cheers\.bio/, /^\//],
-        }),
-    ],
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 0.2,
-});
+// Sentry.init({
+//     app,
+//     dsn: 'https://19adad3122ef486cbd3201112af00024@sentry.rss3.dev/2',
+//     integrations: [
+//         new Integrations.BrowserTracing({
+//             routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+//             tracingOrigins: [/^(.+\.)?cheers\.bio/, /^\//],
+//         }),
+//     ],
+//     // Set tracesSampleRate to 1.0 to capture 100%
+//     // of transactions for performance monitoring.
+//     // We recommend adjusting this value in production
+//     tracesSampleRate: 0.2,
+// });
 /* FITRUE_isNDevelop */
 
 app.mount('#app');
