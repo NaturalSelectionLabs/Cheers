8e9fbeab86e80a1299335c18f316b319519268fa
azuic <zuic10a@gmail.com>
2022-03-18 20:38:06 +0800

    fix: provider url

--- src/views/Capsule.vue
+++ src/views/Capsule.vue
@@ -150,10 +150,10 @@ export default class Capsule extends Vue {
         }
 
         let abi = ['function mint(address receiver, bytes32 mHash, uint tsp, uint expiry, bytes memory sig)'];
-        let contractAddress = '0x999017cb5652caf5f324a8e44f813903ba3c46eb'; // bsc testnet
-        // let contractAddress = ''; // TODO: bsc mainnet
-        let url = 'https://data-seed-prebsc-1-s1.binance.org:8545'; // bsc testnet
-        // let url = 'bsc-dataseed.binance.org:8545'; // bsc mainnet
+        // let contractAddress = ''; // bsc testnet
+        let contractAddress = '0x999017cb5652caf5f324a8e44f813903ba3c46eb'; // TODO: bsc mainnet
+        // let url = 'https://data-seed-prebsc-1-s1.binance.org:8545'; // bsc testnet
+        let url = 'bsc-dataseed.binance.org:8545'; // bsc mainnet
 
         let provider = new ethers.providers.JsonRpcProvider(url);
         let contract = new ethers.Contract(contractAddress, abi, this.wallet);
