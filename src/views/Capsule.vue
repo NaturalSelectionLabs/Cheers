cc4cbad80a663c891d10cd0b081bdc9972371a17
azuic <zuic10a@gmail.com>
2022-03-18 21:00:42 +0800

    fix: rpc link

--- src/views/Capsule.vue
+++ src/views/Capsule.vue
@@ -151,9 +151,9 @@ export default class Capsule extends Vue {
 
         let abi = ['function mint(address receiver, bytes32 mHash, uint tsp, uint expiry, bytes memory sig)'];
         // let contractAddress = ''; // bsc testnet
-        let contractAddress = '0x999017cb5652caf5f324a8e44f813903ba3c46eb'; // TODO: bsc mainnet
+        let contractAddress = '0x999017cb5652caf5f324a8e44f813903ba3c46eb'; // bsc mainnet
         // let url = 'https://data-seed-prebsc-1-s1.binance.org:8545'; // bsc testnet
-        let url = 'bsc-dataseed.binance.org:8545'; // bsc mainnet
+        let url = 'bsc-dataseed.binance.org'; // bsc mainnet
 
         let provider = new ethers.providers.JsonRpcProvider(url);
         let contract = new ethers.Contract(contractAddress, abi, this.wallet);
