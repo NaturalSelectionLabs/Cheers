22aab736ab6fed1367bc519465c7a09abf827c8b
azuic <zuic10a@gmail.com>
2022-03-18 23:36:03 +0800

    feat: error messages when creating a capsule

--- src/views/Capsule.vue
+++ src/views/Capsule.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="fixed h-full w-full overflow-x-hidden bg-paper px-36 py-36">
+    <div class="h-full w-full bg-paper px-36 py-36">
         <div class="flex flex-col items-start gap-20">
             <h1 class="font-cormorant text-6xl">Capsule</h1>
             <!-- On Entering -->
@@ -38,13 +38,14 @@
                 </div>
                 <div class="flex w-full flex-row items-start gap-4 font-cormorant font-medium">
                     <div class="w-32 pt-3">Open at</div>
-                    <div class="w-full">
+                    <div class="flex w-full gap-4">
                         <input
-                            class="input cursor-pointer rounded-xl border-input border-input-border bg-card-bg pb-3 pl-5 pr-5 pt-3 font-notoSerif placeholder-black placeholder-opacity-20 outline-none"
+                            class="input cursor-pointer rounded-xl border-input border-input-border bg-card-bg pb-3 pl-5 pr-5 pt-3 font-notoSerif placeholder-black placeholder-opacity-20 outline-none out-of-range:bg-error out-of-range:bg-opacity-40"
                             type="datetime-local"
                             v-model="openTsp"
                             :min="minTsp"
                         />
+                        <div class="pt-3 opacity-40">Please enter a time at least one year from now.</div>
                     </div>
                 </div>
                 <div class="flex w-full flex-row items-start gap-4 font-cormorant font-medium">
@@ -59,10 +60,12 @@
                 <Button
                     size="lg"
                     class="h-14 w-90 rounded-xs bg-black font-cormorant text-2xl font-medium text-white"
+                    :class="{ disabled: isSubmitted }"
                     @click="createCapsule"
                 >
-                    <span class="w-80 text-left">Create</span>
+                    <span class="w-80 text-left">{{ isSubmitted ? 'Creating...' : 'Create' }}</span>
                 </Button>
+                <div class="text-error">{{ notice }}</div>
             </div>
 
             <!-- On Completing -->
@@ -106,6 +109,7 @@ export default class Capsule extends Vue {
     minTsp: string = '';
     notice: string = '';
     txn: string = '';
+    isSubmitted: boolean = false;
 
     verifyWithCaptcha = setup(() => {
         const reCaptcha = useReCaptcha();
@@ -131,56 +135,66 @@ export default class Capsule extends Vue {
     }
 
     async createCapsule() {
-        const captchaToken = await this.verifyWithCaptcha.exec();
-        console.log(captchaToken);
-
-        const tsp = Math.ceil(new Date(this.openTsp).valueOf() / 1000);
-        const res = await axios.post(`https://capsule.cheer.bio/mint/`, {
-            minter: this.walletAddress,
-            receiver: this.recipient,
-            words: this.message,
-            tsp,
-            reCaptcha: captchaToken,
-        });
-        // console.log(res.data);
-        if (res.data.error) {
-            //TODO
-            console.log(res.data.error);
+        if (this.openTsp < this.minTsp) {
+            this.notice = 'Please re-enter a time at least one year from now.';
             return;
-        }
+        } else {
+            const captchaToken = await this.verifyWithCaptcha.exec();
+            // console.log(captchaToken);
 
-        let abi = ['function mint(address receiver, bytes32 mHash, uint tsp, uint expiry, bytes memory sig)'];
-        // let contractAddress = ''; // bsc testnet
-        let contractAddress = '0x999017cb5652caf5f324a8e44f813903ba3c46eb'; // bsc mainnet
-        // let url = 'https://data-seed-prebsc-1-s1.binance.org:8545'; // bsc testnet
-        let url = 'https://bsc-dataseed.binance.org'; // bsc mainnet
+            const tsp = Math.ceil(new Date(this.openTsp).valueOf() / 1000);
+            const res = await axios.post(`https://capsule.cheer.bio/mint/`, {
+                minter: this.walletAddress,
+                receiver: this.recipient,
+                words: this.message,
+                tsp,
+                reCaptcha: captchaToken,
+            });
+            // console.log(res.data);
+            if (res.data.error) {
+                //TODO
+                // console.log(res.data.error);
+                if (res.data.error === 'Invalid token.') {
+                    this.notice = 'Something went wrong. Please try again.';
+                } else {
+                    this.notice = res.data.error;
+                }
+                return;
+            }
+            this.isSubmitted = true;
+            let abi = ['function mint(address receiver, bytes32 mHash, uint tsp, uint expiry, bytes memory sig)'];
+            // let contractAddress = ''; // bsc testnet
+            let contractAddress = '0x999017cb5652caf5f324a8e44f813903ba3c46eb'; // bsc mainnet
+            // let url = 'https://data-seed-prebsc-1-s1.binance.org:8545'; // bsc testnet
+            let url = 'https://bsc-dataseed.binance.org'; // bsc mainnet
 
-        let provider = new ethers.providers.JsonRpcProvider(url);
-        let contract = new ethers.Contract(contractAddress, abi, this.wallet);
+            let provider = new ethers.providers.JsonRpcProvider(url);
+            let contract = new ethers.Contract(contractAddress, abi, this.wallet);
 
-        if (this.wallet) {
-            try {
-                const contractSigned = contract.connect(provider);
-                const txData = await contractSigned.populateTransaction.mint(
-                    this.recipient,
-                    res.data.msgHash,
-                    tsp,
-                    res.data.expiry,
-                    res.data.sig,
-                );
-                // console.log('tx', txData);
-                const tx = await this.wallet.signTransaction({
-                    ...txData,
-                    gasLimit: 200000,
-                    gasPrice: await provider.getGasPrice(),
-                });
-                const r = await provider.sendTransaction(tx);
+            if (this.wallet) {
+                try {
+                    const contractSigned = contract.connect(provider);
+                    const txData = await contractSigned.populateTransaction.mint(
+                        this.recipient,
+                        res.data.msgHash,
+                        tsp,
+                        res.data.expiry,
+                        res.data.sig,
+                    );
+                    // console.log('tx', txData);
+                    const tx = await this.wallet.signTransaction({
+                        ...txData,
+                        gasLimit: 180000,
+                        gasPrice: 5000000000,
+                    });
+                    const r = await provider.sendTransaction(tx);
 
-                // console.log(r.hash);
-                this.isCreated = true;
-                this.txn = r.hash;
-            } catch (e) {
-                console.log(e);
+                    // console.log(r.hash);
+                    this.isCreated = true;
+                    this.txn = r.hash;
+                } catch (e) {
+                    console.log(e);
+                }
             }
         }
     }
@@ -194,5 +208,12 @@ export default class Capsule extends Vue {
 <style scoped lang="postcss">
 .input::placeholder {
     font-weight: 500;
+}
+
+.disabled {
+    cursor: not-allowed;
+    pointer-events: none;
+
+    opacity: 0.5;
 }
 </style>
