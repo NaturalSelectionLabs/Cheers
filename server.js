c7d94b2ca4d25f62fea6516bdef7a907f699a938
Nya Candy <dev@candinya.com>
2022-09-19 12:19:02 +0800

    feat: just redirect

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const CORS = require('@koa/cors');
 const koaViews = require('koa-views');
 const koaStatic = require('koa-static');
 const path = require('path');
-const axios = require('axios');
 // const compress = require('koa-compress');
 const { default: sslify, xForwardedProtoResolver } = require('koa-sslify');
 
@@ -27,30 +26,11 @@ app.use(
     }),
 );
 
-const getNameByAddress = async (address) => {
-    try {
-        const name = (await axios.get(`https://rss3.domains/address/${address}`)).data;
-        return name?.rnsName.replace('.rss3', '') || name?.dasName || name?.ensName || '';
-    } catch (e) {
-        console.log(e);
-        return '';
-    }
-};
-
 const getName = async (host, url) => {
     if (host.split('.').length > 2) {
         return host.split('.').slice(0, -2).join('.');
     } else {
         return /^\/(.+?)\//.exec(url + '/')?.[1];
-    }
-};
-
-const getAddress = async (name) => {
-    try {
-        return (await axios.get(`https://rss3.domains/name/${name}`)).data.address;
-    } catch (e) {
-        console.log(e);
-        return '';
     }
 };
 
@@ -61,20 +41,9 @@ const injectMetadata = async (ctx) => {
     const host = ctx.host;
     const aon = await getName(host, ctx.url);
 
-    let address = '';
-    if (aon) {
-        // get address
-        if (/^0x[a-fA-F0-9]{40}$/.test(aon)) {
-            // Is address
-            address = aon;
-        } else {
-            address = await getAddress(aon);
-        }
-    }
-
     // Redirect to RSS3.io
-    if (address && parseInt(address, 16) !== 0) {
-        await ctx.redirect(`https://rss3.io/result?search=${address}`);
+    if (aon) {
+        await ctx.redirect(`https://rss3.io/result?search=${aon}`);
     } else {
         await ctx.redirect('https://rss3.io/');
     }
